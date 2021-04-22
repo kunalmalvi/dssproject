543be487d7e7e0c98646855f0723f14d7d509144
kunalmalvi <malvi.kunal67@gmail.com>
2021-04-22 15:33:12 +0530

    update

--- DSS_App/forms.py
+++ DSS_App/forms.py
@@ -3,3 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from django import forms
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = ["first_name","last_name","acc_number","email","phone","balance"]
--- DSS_App/views.py
+++ DSS_App/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from .forms import *
 
 
 @login_required
