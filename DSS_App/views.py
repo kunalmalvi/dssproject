6eda0505e31b48a7b0a93b97e97401fda5221c21
kunalmalvi <malvi.kunal67@gmail.com>
2021-04-22 15:46:52 +0530

    customer profile fix

--- DSS_App/views.py
+++ DSS_App/views.py
@@ -10,6 +10,11 @@ def home(request):
     title = "Online Fund Transfer"
     return render(request, 'index.html')
 
-def customer_profile(request, pk):
-    vs = get_object_or_404(Customer, pk=pk)
-    return render(request, 'customer_profile.html', {'object': vs})
+def customer_profile(request):
+    customer_obj = Customer.objects.all()
+    for i in range(len(Student_obj)):
+        if request.user.id == customer_obj[i].user_id:
+            key=customer_obj[i].c_id
+            vs = get_object_or_404(Customer, pk=key)
+            return render(request, 'customer_profile.html', {'object': vs})
+    # return render(request, 'profile_not_found.html')
