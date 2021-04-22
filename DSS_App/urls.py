dae1b69d3dab2eb472af79bab015dbe3f1105f7c
kunalmalvi <malvi.kunal67@gmail.com>
2021-04-22 15:57:23 +0530

    customer profile fix

--- DSS_App/urls.py
+++ DSS_App/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
 	path('', home,name='home'),
-	path('customer_profile/<int:pk>',customer_profile,name='customer_profile'),
+	path('customer_profile',customer_profile,name='customer_profile'),
 ]
