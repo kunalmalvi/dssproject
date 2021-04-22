2455f452f2043423b91aafa31dcc78d1902c4309
kunalmalvi <malvi.kunal67@gmail.com>
2021-04-22 15:19:01 +0530

    Added Customers Profile

--- DSS_App/urls.py
+++ DSS_App/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
 	path('', home,name='home'),
-	path('student_profile/<int:pk>',student_profile,name='student_profile'),
+	path('customer_profile/<int:pk>',customer_profile,name='customer_profile'),
 ]
